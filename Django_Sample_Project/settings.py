32df9a91f4bcc54677cc572e70a559fa6a8cd2f5
abhayb443 <pandeyabhay443@gmail.com>
2020-08-21 00:38:28 +0530

    settings updated

--- Django_Sample_Project/settings.py
+++ Django_Sample_Project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = SECRET_KEY
+SECRET_KEY = ')8(_2c61ykggywh*)nae=u*ei^-x4=mfrmqf&ma#!op0s49!$_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
