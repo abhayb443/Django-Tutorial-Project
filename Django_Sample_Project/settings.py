e2de2bd9ccc709baa82b952933a784c42973f243
abhayb443 <pandeyabhay443@gmail.com>
2020-09-06 16:48:24 +0530

    Db info updated ....

--- Django_Sample_Project/settings.py
+++ Django_Sample_Project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-import dj-database-url
+import dj_database_url
 import os
 
 # from secret_settings import *
